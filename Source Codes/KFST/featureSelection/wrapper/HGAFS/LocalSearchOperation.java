e71ed12d86af640004bb154c3f7ab081d293e96d
shade-black <shadi.barazandeh@gmail.com>
2017-07-06 15:46:33 +0430

    final lso

--- Source Codes/KFST/featureSelection/wrapper/HGAFS/LocalSearchOperation.java
+++ Source Codes/KFST/featureSelection/wrapper/HGAFS/LocalSearchOperation.java
@@ -41,50 +41,73 @@ public class LocalSearchOperation {
 
     public Strings lso(Strings offSpring) {
         int[] X = offSpring.getOnes();
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> Xd = new ArrayList<Integer>();
         for (int i = 0; i < D.length; i++) {
             for (int j = 0; j < X.length; j++) {
                 if (D[i] == X[j]) {
-                    list.add(D[i]);
+                    Xd.add(D[i]);
                 }
             }
         }
-        Integer[] Xd = new Integer[list.size()];
-        Xd = list.toArray(Xd);
-        list.clear();
+
+        ArrayList<Integer> Xs = new ArrayList<Integer>();
         for (int i = 0; i < S.length; i++) {
             for (int j = 0; j < X.length; j++) {
                 if (S[i] == X[j]) {
-                    list.add(S[i]);
+                    Xs.add(S[i]);
                 }
             }
         }
-        Integer[] Xs = new Integer[list.size()];
-        Xs = list.toArray(Xs);
+        if (delta > Xd.size()) {
+            for (int i = 0 , j=0 ; j < delta - Xd.size() & i< D.length ;i++) {
+                //addToXd
+                Boolean exist=false;
+                for (int k = 0 ; k < Xd.size();k++) {
+                   if(D[i]==Xd.get(k)){
+                       exist=true;
+                       break;
+                   }
+                }
+                if(!exist){
+                    Xd.add(D[i]);
+                    j++;
+                }
 
-        if (delta > Xd.length) {
-            for (int i = 0; i < delta - Xd.length; i++) {
-                addToXd();
             }
         }
 
-        if (delta < Xd.length) {
-            for (int i = 0; i < Xd.length - delta; i++) {
-                delFromXd();
+        if (delta < Xd.size()) {
+            for (int i = 0, j=Xd.size()-1;j>0 & i < Xd.size() - delta; j--,i++) {
+                //delFromXd
+                Xd.remove(j);
             }
         }
 
-        if (zi > Xs.length) {
-            for (int i = 0; i < delta - Xs.length; i++) {
-                addtoXs();
+        if (zi > Xs.size()) {
+            for (int i = 0 , j=0 ; j < zi - Xs.size() & i< S.length ;i++) {
+                //addToXs
+                Boolean exist=false;
+                for (int k = 0 ; k < Xs.size();k++) {
+                    if(S[i]==Xs.get(k)){
+                        exist=true;
+                        break;
+                    }
+                }
+                if(!exist){
+                    Xs.add(S[i]);
+                    j++;
+                }
+
             }
         }
 
-        if (zi < Xd.length) {
-            for (int i = 0; i < Xd.length - delta; i++) {
-                delFromXs();
+        if (zi < Xs.size()) {
+            for (int i = 0, j=Xs.size()-1;j>0 & i < Xs.size() - zi; j--,i++) {
+                //delFromXs
+                Xs.remove(j);
             }
         }
+
 
 
         return offSpring;
