8fcf01db6d605d83b41e715c19d65af2bc30fba1
sina <sinashaloudegi@gmail.com>
2017-07-13 13:02:31 +0430

    HPSO-LS Set up2

--- Source Codes/KFST/featureSelection/wrapper/HPSOLS/HPSOLSMain.java
+++ Source Codes/KFST/featureSelection/wrapper/HPSOLS/HPSOLSMain.java
@@ -1,19 +1,32 @@
 package KFST.featureSelection.wrapper.HPSOLS;
 
 import KFST.dataset.DatasetInfo;
+import KFST.featureSelection.wrapper.PSO.Swarm;
 import KFST.featureSelection.wrapper.WrapperApproach;
 
 /**
  * Created by sina on 7/13/2017.
  */
 public class HPSOLSMain implements WrapperApproach {
-    public HPSOLSMain(int numSelectedSubset, int numSwarmPopulation, int numIterates) {
+    Swarm swarm;
+    int numIterates;
+    int numFeatures;
+    int numSwarmPopulation;
+    int numSelectedFeatures;
+    String pathData;
+    String pathTestData;
 
+    public HPSOLSMain(int numSelectedFeatures, int numSwarmPopulation, int numIterates) {
+        this.numIterates = numIterates;
+        this.numSwarmPopulation = numSwarmPopulation;
+        this.numSelectedFeatures = numSelectedFeatures;
     }
 
     @Override
     public void loadDataSet(DatasetInfo ob) {
-
+        numFeatures = ob.getNumFeature();
+        pathData = ob.getPathData();
+        pathTestData = ob.getPathTestSet();
     }
 
     @Override
